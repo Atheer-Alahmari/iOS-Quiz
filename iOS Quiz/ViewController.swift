402af6dc32ee0987a77dbbd2423790067f962e86
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-01 14:55:08 +0300

    Initial Commit

---
+++ iOS Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Quiz
+//
+//  Created by Atheer Alahmari on 26/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
